2a10a949fd37051f76ee8356a416c9a95a588f86
mefix007 <111250333+mefix007@users.noreply.github.com>
2024-10-02 23:43:45 +0100

    directory error fixed

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -18,6 +18,21 @@ menu.addEventListener('click', function () {
     navigation.classList.toggle('show');
 });
 
+// calculation for windchill
+const temp = 26;
+const windSpeed = 5;
+function calculateWindchill(temp, windSpeed) {
+    if (temp <= 10 && windSpeed > 4.8) {
+        const windchill = 35.74 + 0.6215 * temp - 35.75 * windSpeed ** 0.16 + 0.4275 * temp * windSpeed ** 0.16;
+        const roundedWindchill = Math.round(windchill * 100) / 100;
+        return roundedWindchill;
+    } else {
+        return "N/A";
+    }
+}
+const windchillFactor = document.querySelector('#windchill-factor');
+windchillFactor.textContent = calculateWindchill(temp, windSpeed);
+
 // Fetch .json file from the data folder   
 const filePath = 'data/members.json';
 
