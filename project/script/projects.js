c097c657083cdf8cb646cfacb2efb8e22ef1fb63
mefix007 <111250333+mefix007@users.noreply.github.com>
2024-10-27 14:19:39 +0100

    all pages initiated

--- project/script/projects.js
+++ project/script/projects.js
@@ -39,3 +39,31 @@ navLinks.forEach(link => {
         addActiveClass(link); // Add active class to the clicked link
     });
 });
+
+
+// script for animated slide show
+// script.js
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    if (n >= slides.length) { slideIndex = 0 }
+    if (n < 0) { slideIndex = slides.length - 1 }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
+
+// Next/previous controls
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+// Automatic slideshow
+setInterval(function () {
+    showSlides(slideIndex += 1);
+}, 5000); // Change image every 3 seconds
