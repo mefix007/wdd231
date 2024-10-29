3dce2473e7eb68c02416445ac65a523e10137dd9
mefix007 <111250333+mefix007@users.noreply.github.com>
2024-10-29 01:36:14 +0100

    project index.html

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -17,31 +17,6 @@ menu.addEventListener('click', function () {
 });
 
 
-// scrit for menu Get all the navigation links
-const navLinks = document.querySelectorAll('.navigation a');
-
-// Function to remove 'active' class from all links
-function removeActiveClasses() {
-    navLinks.forEach(link => {
-        link.classList.remove('active');
-    });
-}
-
-// Function to add 'active' class to the clicked link
-function addActiveClass(clickedLink) {
-    removeActiveClasses(); // Remove the active class from all links first
-    clickedLink.classList.add('active'); // Add active class to the clicked link
-}
-
-// Add event listeners to each nav link
-navLinks.forEach(link => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault(); // Prevent the default link behavior (optional, if you don’t want to navigate)
-        addActiveClass(link); // Add active class to the clicked link
-    });
-});
-
-
 // select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
--- project/script/projects.js
+++ project/script/projects.js
@@ -16,30 +16,6 @@ menu.addEventListener('click', function () {
 });
 
 
-// scrit for menu Get all the navigation links
-const navLinks = document.querySelectorAll('.navigation a');
-
-// Function to remove 'active' class from all links
-function removeActiveClasses() {
-    navLinks.forEach(link => {
-        link.classList.remove('active');
-    });
-}
-
-// Function to add 'active' class to the clicked link
-function addActiveClass(clickedLink) {
-    removeActiveClasses(); // Remove the active class from all links first
-    clickedLink.classList.add('active'); // Add active class to the clicked link
-}
-
-// Add event listeners to each nav link
-navLinks.forEach(link => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault(); // Prevent the default link behavior (optional, if you don’t want to navigate)
-        addActiveClass(link); // Add active class to the clicked link
-    });
-});
-
 
 // script for animated slide show
 // script.js
@@ -66,4 +42,4 @@ function changeSlide(n) {
 // Automatic slideshow
 setInterval(function () {
     showSlides(slideIndex += 1);
-}, 5000); // Change image every 3 seconds
+}, 3000); // Change image every 3 seconds
